27923df89ac45039c7acac7d25c50783f358b560
tstike44 <you@example.com>
2019-04-27 00:28:01 -0700

    started retrieving match data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,16 @@
 var db = require("../models");
+const OverwatchLeague = require('overwatchleague');
+const OWL = new OverwatchLeague();
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+
+module.exports = function (app) {
+  app.get("/api/owlteam", function (req, res) {
+    //get all owl events
+    OWL.getWeeksMatches(req).then(response => {
+      console.log(response.data);
+      res.json(response)
     });
   });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-};
+  
+ 
+}
