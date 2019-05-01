47af043c20b85a03cca556205448e83af97cafcb
Trevor <tstike44@hotmail.com>
2019-04-30 18:10:53 -0700

    moving to desktop

--- app/public/js/bets.js
+++ app/public/js/bets.js
@@ -1,8 +1,20 @@
-let userBet;
+// Get references to page elements
+var $teamOne = $("#team-1");
 
-// function matchOne(){
-// if (userBet === ) {
+// The API object contains methods for each kind of request we'll make
+var API = {
+    saveBet: function (bet) {
+        return $.ajax({
+            headers: {
+                "Content-Type": "application/json"
+            },
+            type: "Get",
+            url: "api/examples",
+            data: JSON.stringify(bet)
+        });
+    }
+};
 
-// }
-// }
+// Add event listeners to the submit and delete buttons
+$teamOne.on("click", saveBet);
 
